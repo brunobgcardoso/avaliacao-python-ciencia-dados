53319d79b75cd60b3aa9cadce43b581e93044a14
Bruno Braga Guedes Cardoso <brunobgcardoso@yahoo.com.br>
2025-10-08 19:53:03 -0300

    Add utils file

---
+++ src/utils/data_info.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def analisa_dados_ausentes(dataframe):    
+    dados_ausentes = dataframe.isna().sum()
+    total = dataframe.shape[0]
+    ausentes = dados_ausentes.reset_index(name="Quantidade")
+    ausentes.rename(columns={"index": "variavel_analisada"}, inplace=True)
+    ausentes['Percentual'] = (((ausentes['Quantidade'] / total)*100).round(2)).apply(lambda x: f'{x:.1f}%')
+    
+    filtro = ausentes['Quantidade'] > 0
+    return ausentes[filtro]
+
+def verifica_dados_duplicados(dataframe):
+    qtd = dataframe.duplicated().sum()
+    print(f'Foram encontrados {qtd} registros duplicados\n')
+    print('Prévia dos dados duplicados:')
+    filtro = dataframe.duplicated()
+    return dataframe[filtro].head(5)
+
+def importa_csv(dataframe_name, separador=";", encoding="utf-8"):
+    dataframe = pd.read_csv(dataframe_name, sep=separador, encoding=encoding)
+    print(f'Dataframe importado com sucesso! \nDimensão: {dataframe.shape}')
+    return dataframe
+
+def junta_dataframes(df1, df2, chave, tipo_juncao='inner'):
+    df_final = pd.merge(df1, df2, on=chave, how=tipo_juncao)
+    print(f'Dataframes unidos com sucesso! \nDimensão: {df_final.shape}')
+    return df_final
